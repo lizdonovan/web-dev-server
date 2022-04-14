185d59b5731cf6e0f3063c2cc0cecfeb38d084ad
Liz Donovan <41712505+lizdonovan@users.noreply.github.com>
2022-04-14 19:31:21 -0400

    update connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from "mongoose";
+
 import helloController from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
@@ -10,5 +16,5 @@ helloController(app);
 app.use(express.json());
 userController(app);
 tuitsController(app);
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 3000);
 
