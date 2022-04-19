12999c51da2fdc9752d7bd754b72d3c6498069a7
donovanel <donovan.el@northeastern.edu>
2022-04-19 08:42:17 -0400

    node version of server.js

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
 
-import helloController from "./controllers/hello-controller.js";
-import userController   from "./controllers/user-controller.js";
-import tuitsController from "./controllers/tuits-controller.js";
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/webdev'
-mongoose.connect(CONNECTION_STRING);
+import helloController
+    from "./controllers/hello-controller.js";
+import userController   from "./controllers/users-controller.js";
 
 const app = express();
 app.use(cors());
-helloController(app);
-app.use(express.json());
-userController(app);
-tuitsController(app);
-app.listen(process.env.PORT || 4000);
 
+app.get('/hello', (req, res) => {res.send('Hello World!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+app.use(express.json());
+helloController(app);
+userController(app);
+app.listen(4000);
