0b1841d9edcf039f40142805c2f10ec3f657aafc
donovanel <donovan.el@northeastern.edu>
2022-04-24 19:01:43 -0400

    missing code from server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 
 import helloController
     from "./controllers/hello-controller.js";
-import userController   from "./controllers/users-controller.js";
+import userController   from "./controllers/user-controller.js";
+import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(function(req,
+                 res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
@@ -14,4 +25,5 @@ app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 app.use(express.json());
 helloController(app);
 userController(app);
+tuitsController(app)
 app.listen(4000);
